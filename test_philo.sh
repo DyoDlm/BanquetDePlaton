607fa8f03096074e3ddb05e3a6926bda7780a6df
DyoDlm <seitealexander@gmail.com>
2025-04-12 13:44:20 -0400

    data races and double lock to handle

--- test_philo.sh
+++ test_philo.sh
@@ -79,16 +79,41 @@ echo "==== Testing Philosopher Program ===="
 #	do_test $ARGS
 #done
 
-# Test <= 50 %
-echo -e "\nTesting <= 50% SUCCESS RATE"
+#	TEST EXAMEN
+#echo -e "\nBasic Tests for exam"
+#echo "Every philospher should live"
+#
+#for ARGS in \
+#	"5 800 200 200 20" \
+#	"4 410 200 200 5" \
+#	"4 410 200 200 10"
+#do
+#	do_test $ARGS
+#done
 
+echo -e "A philosopher should die"
+do_test 4 310 200 100 10
+
+echo -e "${YELLOW}Sketchy tests"
+# Min 60 ms for time to EAT/DIE/SLEEP
+# Max 200 philosophers
 for ARGS in \
-	"50 401 200 200 10" \
-	"198 401 200 200 10" 
+	"3 60 10 10 1000"
 do
 	do_test $ARGS
 done
 
-# Test 0%
-echo -e "\nTesting 0% SUCCESS RATE"
-do_test 130 401 200 200 10
+# Test <= 50 %
+#echo -e "\nTesting <= 50% SUCCESS RATE"
+#
+#for ARGS in \
+#	"50 401 200 200 10" \
+#	"198 401 200 200 10" 
+#do
+#	do_test $ARGS
+#done
+#
+## Test 0%
+#echo -e "\nTesting 0% SUCCESS RATE"
+#do_test 130 401 200 200 10
+#
